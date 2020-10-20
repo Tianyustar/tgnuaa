b94de315a80b8a90770e1bd86737544842c652ec
TianyuStar <yunxing.liu@foxmail.com>
2020-10-20 15:18:29 +0800

    对接case接口

--- src/api/search.js
+++ src/api/search.js
@@ -7,3 +7,13 @@ export function searchKeyWords(userRole) { // 用户身份
         params:userRole
     })
 }
+
+export function searchCase(keyword) { // 获取案例
+   
+   
+    return request({
+        url:'/keyword/cases',
+        method:'get',
+        params:{keyword}
+    })
+}
--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,86 +1,155 @@
 <template>
   <div class="search-page">
-
     <header class="search-head">
-           <el-row>
-             <div class="title">  航 空 慧 法
-      </div>
-      
-      <el-input
-        v-model="searchContent"
-        placeholder="输入内容"
-        class="search-input"
-      ></el-input>
-      <el-button type="primary" class="search-button">搜索</el-button>
-      <el-button
-        type="primary"
-        class="search-button"
-        @click="screenShow = !screenShow"
-        >{{ screenShow ? "收起筛选" : "展开筛选" }}</el-button
-      > </el-row>
-    </header>
-  <el-main>
-    <el-row >
-      <div v-if="screenShow" class="shaixuan">
-        <el-table
-          :data="tableData"
-          border
-          style="width: 100%"
-          :show-header="false"
-        >
-          <el-table-column prop="date" label="item" width="180" align="center">
-            <template slot-scope="scope">
-              <div class="table-item">{{ scope.row.item }}</div>
-            </template>
-          </el-table-column>
-          <el-table-column prop="name" label="content">
-            <template slot-scope="scope">
-              <span
-                v-for="(item, index) in scope.row.content"
-                :key="index"
-                class="content-item"
-                @click="showContentList(item)"
-              >
-                <span>{{ item.text }}</span>
-                <span class="item-num">({{ item.num }})</span>
-              </span>
-              <!-- <div class="table-item">{{scope.row.content}}</div> -->
-            </template>
-          </el-table-column>
-        </el-table>
-      </div>
-    </el-row>
+      <el-row>
+        <div class="title">航 空 慧 法</div>
 
-    <div class="content-list">
-      <el-card class="box-card" v-for="(item, index) in itemList" :key="index">
-        <div slot="header" class="clearfix">
-          <span>{{ item.title }}</span>
+        <el-input
+          v-model="searchContent"
+          placeholder="输入内容"
+          class="search-input"
+        ></el-input>
+        <el-button type="primary" class="search-button">搜索</el-button>
+        <el-button
+          type="primary"
+          class="search-button"
+          @click="screenShow = !screenShow"
+          >{{ screenShow ? "收起筛选" : "展开筛选" }}</el-button
+        >
+      </el-row>
+    </header>
+    <el-main>
+      <el-row>
+        <div v-if="screenShow" class="shaixuan">
+          <el-table
+            :data="tableData"
+            border
+             v-loading="table1loading"
+            style="width: 100%"
+            :show-header="false"
+          >
+            <el-table-column
+              prop="date"
+              label="item"
+              width="180"
+              align="center"
+            >
+              <template slot-scope="scope">
+                <div class="table-item">{{ scope.row.name }}</div>
+              </template>
+            </el-table-column>
+            <el-table-column prop="name" label="content">
+              <template slot-scope="scope">
+                <template v-for="(item, index) in scope.row.children">
+                  <span
+                    :key="index"
+                    class="content-item"
+                    @click="showContentList(item)"
+                    v-if="scope.row.showMore ? true : index < 8"
+                  >
+                    <span>{{ item.name }}</span>
+                    <span class="item-num">({{ item.id }})</span>
+                    <!-- <span>|</span> -->
+                  </span>
+                </template>
+                <el-button 
+                  v-if="scope.row.children.length > 8"
+                  :type="scope.row.showMore  ? 'warning' : 'primary'"
+                  size="small"
+                  @click="showMoreKeyWords(scope.$index, scope.row.showMore)"
+                  round
+                  >{{ scope.row.showMore ? "收起" : "更多" }}</el-button>
+                <!-- <div class="table-item">{{scope.row.content}}</div> -->
+              </template>
+            </el-table-column>
+          </el-table>
         </div>
-        <el-row class="list-item">
-          <span >基本事实：</span
-          ><span
-            ><el-tag class="item-tag" v-for="(tr, index) in item.baseTruth" :key="index">{{
-              tr
-            }}</el-tag></span
+           <div v-if="screenShow && showThree" class="shaixuan">
+          <el-table
+            :data="threeCatalogue"
+            border
+            style="width: 100%; margin-top 10px"
+            :show-header="false"
           >
-        </el-row>
-        <el-row class="list-item">
-          <span >争议焦点:</span
-          ><span
-            ><el-tag class="item-tag" v-for="(f, index) in item.focuse" :key="index">{{
-              f
-            }}</el-tag></span
-          >
-        </el-row>
-        <el-row>{{ item.content }} </el-row>
-      </el-card>
-    </div>
+            <el-table-column
+              prop="date"
+              label="item"
+              width="180"
+              align="center"
+            >
+              <template slot-scope="scope">
+                <div class="table-item">{{ scope.row.name }}</div>
+              </template>
+            </el-table-column>
+            <el-table-column prop="name" label="content">
+              <template slot-scope="scope">
+                <template v-for="(item, index) in scope.row.children">
+                  <span
+                    :key="index"
+                    class="content-item"
+                    @click="justShowCase(item)"
+                    v-if="scope.row.showMore ? true : index < 8"
+                  >
+                    <span>{{ item.name }}</span>
+                    <span class="item-num">({{ item.id }})</span>
+                    <!-- <span>|</span> -->
+                  </span>
+                </template>
+                <el-button 
+                  v-if="scope.row.children.length > 8"
+                  :type="scope.row.showMore  ? 'warning' : 'primary'"
+                  size="small"
+                  @click="showMoreKeyWords(scope.$index, scope.row.showMore)"
+                  round
+                  >{{ scope.row.showMore ? "收起" : "更多" }}</el-button>
+                <!-- <div class="table-item">{{scope.row.content}}</div> -->
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+      </el-row>
+
+      <div class="content-list">
+        <el-card
+          class="box-card"
+          v-for="(item, index) in itemList"
+          :key="index"
+        >
+          <div slot="header" class="clearfix">
+            <span>{{ item.title }}</span>
+          </div>
+          <el-row class="list-item">
+            <span>基本事实：</span
+            ><span
+              ><el-tag
+                class="item-tag"
+                v-for="(tr, index) in item.baseTruth"
+                :key="index"
+                >{{ tr }}</el-tag
+              ></span
+            >
+          </el-row>
+          <el-row class="list-item">
+            <span>争议焦点:</span
+            ><span
+              ><el-tag
+                class="item-tag"
+                v-for="(f, index) in item.focuse"
+                :key="index"
+                >{{ f }}</el-tag
+              ></span
+            >
+          </el-row>
+          <el-row>{{ item.content }} </el-row>
+        </el-card>
+      </div>
     </el-main>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import { searchKeyWords, searchCase } from "@/api/search";
 
 export default {
   name: "Dashboard",
@@ -88,78 +157,40 @@ export default {
     ...mapGetters(["name", "userRole"])
   },
   mounted() {
-    this.getKeyWordList()
+    this.getKeyWordList();
   },
-  watch:{
-    userRole:function(val) {
-      this.getKeyWordList()
+  watch: {
+    userRole: function(val) {
+      this.getKeyWordList();
     }
-  }
-  ,
+  },
   data() {
     return {
       searchContent: "",
       screenShow: true,
+      table1loading:false,
+      threeCatalogue: [
+        {
+          name: "责任主体的特殊性规定",
+          children: [
+            { name: "非机动车驾驶人责任", num: 3098 },
+            { name: "行人责任", num: 63154 },
+            { name: "挂靠关系", num: 196328 },
+            { name: "挂靠关系", num: 196328 },
+            { name: "劳务关系认定", num: 2028 }
+          ]
+        }
+      ],
+      showThree:false,
       tableData: [
         {
-          item: "责任主体的特殊性规定",
-          content: [
-            { text: "非机动车驾驶人责任", num: 3098 },
-            { text: "行人责任", num: 63154 },
-            { text: "挂靠关系", num: 196328 },
-            { text: "挂靠关系", num: 196328 },
-            { text: "劳务关系认定", num: 2028 }
-          ]
-        },
-        {
-          item: "机动车侵权",
-          content: [
-            { text: "套牌", num: 3098 },
-            { text: "违法通行", num: 17136 },
-            { text: "驾驶人故意制造交通事故", num: 1609 },
-            { text: "盗抢期间造成交通事故", num: 599 },
-            { text: "买卖拼装，报废车辆", num: 775 },
-            { text: "违法驾驶", num: 277471 }
-          ]
-        },
-        {
-          item: "不承担或减轻责任的情形",
-          content: [
-            { text: "受害人过错", num: 14377 },
-            { text: "受害人自担部分责任", num: 45572 },
-            { text: "尽到监护责任", num: 3152 },
-            { text: "紧急避险", num: 1728 },
-            { text: "正当防卫", num: 30 },
-            { text: "因第三人造成的侵权", num: 3903 },
-            { text: "受害人自甘冒险", num: 199 }
-          ]
-        },
-        {
-          item: "法律责任承担",
-          content: [
-            { text: "承担连带责任", num: 276033 },
-            { text: "交通事故责任认证争议", num: 536262 },
-            { text: "共同承担责任", num: 33241 },
-            { text: "承担责任主体的认定", num: 186886 },
-            { text: "无责任赔偿", num: 68723 },
-            { text: "连带责任人之间的追偿", num: 19 }
-          ]
-        },
-        {
-          item: "赔偿费用问题",
-          content: [
-            { text: "交通险的赔偿范围", num: 1528468 },
-            { text: "第三者责任商业保险的赔偿范围", num: 634844 },
-            { text: "未投保交强险", num: 17223 },
-            { text: "赔偿车辆重置费", num: 1621 }
-          ]
-        },
-        {
-          item: "程序问题",
-          content: [
-            { text: "涉外事故", num: 293 },
-            { text: "虚假诉讼", num: 248 },
-            { text: "诉讼主体资格", num: 54041 }
+          name: "责任主体的特殊性规定",
+          children: [
+            { name: "非机动车驾驶人责任", num: 3098 },
+            { name: "行人责任", num: 63154 },
+            { name: "挂靠关系", num: 196328 },
+            { name: "挂靠关系", num: 196328 },
+            { name: "劳务关系认定", num: 2028 }
           ]
         }
       ],
@@ -176,11 +207,36 @@ export default {
   },
   methods: {
     showContentList(item) {
-      console.log(item)
+      if (item.children && item.children.length > 0) {
+        // 有三级目录，显示三级目录
+        this.showThree = true
+      } else {
+        this.showThree = false
+        // 没有三级目录,请求case数据
+        this.justShowCase(item)
+      }
+    },
+    justShowCase(item) {
+      let keywords = item.name;
+      searchCase(keywords).then(res=>{
+        console.log(res)
+      })
     },
     getKeyWordList() {
-      console.log(this.userRole)
-    }
+      this.table1loading = true
+      searchKeyWords({identity :this.userRole}).then(res => {
+        this.table1loading = false
+        if (res && res.data && res.data.children) {
+          this.tableData = res.data.children;
+        }
+      }); // 获得关键词列表
+    },
+    showMoreKeyWords(index, value) {
+      let newObj = this.tableData[index];
+      newObj.showMore = !value;
+      this.$set(this.tableData, index, newObj);
+    },
+
   }
 };
 </script>
@@ -232,8 +288,12 @@ export default {
 }
 .title {
   margin-bottom: 40px;
-  text-align: center ;
+  text-align: center;
   font-size: 35px;
   color: white;
+}
+.show-more-button {
+  color: rgb(85, 104, 182);
+  cursor: pointer;
 }
 </style>
